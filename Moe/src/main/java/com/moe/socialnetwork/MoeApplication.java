76bf9806c524e025dc1be7b81ae5de59ff515877
nhut347927 <nhut2846@gmail.com>
2025-07-11 22:18:45 +0700

    fix

--- Moe/src/main/java/com/moe/socialnetwork/MoeApplication.java
+++ Moe/src/main/java/com/moe/socialnetwork/MoeApplication.java
@@ -1,22 +1,29 @@
 package com.moe.socialnetwork;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-/**
- * Author: nhutnm379
- */
 @SpringBootApplication
 public class MoeApplication {
 
-	public static void main(String[] args) {
-		  // Load .env vào System properties
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
-		SpringApplication.run(MoeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Chỉ load .env khi không chạy test
+        if (!isTestEnvironment()) {
+            Dotenv dotenv = Dotenv.configure()
+                                  .ignoreIfMissing()
+                                  .load();
 
+            dotenv.entries().forEach(entry -> {
+                System.setProperty(entry.getKey(), entry.getValue());
+            });
+        }
+
+        SpringApplication.run(MoeApplication.class, args);
+    }
+
+    private static boolean isTestEnvironment() {
+        String env = System.getProperty("ENV");
+        return env != null && env.equalsIgnoreCase("test");
+    }
 }
--- Moe/src/test/java/com/moe/socialnetwork/MoeApplicationTests.java
+++ Moe/src/test/java/com/moe/socialnetwork/MoeApplicationTests.java
@@ -1,13 +1,18 @@
 package com.moe.socialnetwork;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MoeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void setup() {
+        System.setProperty("ENV", "test");
+    }
 
+    @Test
+    void contextLoads() {
+    }
 }
