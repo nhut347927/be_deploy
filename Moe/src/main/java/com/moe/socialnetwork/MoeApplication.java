e0c4c408a849daa789ee914c3a70eb5f6b473364
nhut347927 <nhut2846@gmail.com>
2025-07-21 14:47:04 +0700

    fix

--- Moe/src/main/java/com/moe/socialnetwork/MoeApplication.java
+++ Moe/src/main/java/com/moe/socialnetwork/MoeApplication.java
@@ -14,10 +14,10 @@ public class MoeApplication {
 
 	public static void main(String[] args) {
 		  // Load .env vào System properties
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
+        // Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        // dotenv.entries().forEach(entry -> {
+        //     System.setProperty(entry.getKey(), entry.getValue());
+        // });
 		SpringApplication.run(MoeApplication.class, args);
 	}
 
